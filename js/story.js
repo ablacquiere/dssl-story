64a04cf5c285255f652498e8c811f7ef68d86779
ablacquiere <ashley.blacquiere@gmail.com>
2017-02-08 13:56:23 -0800

    First submit
    First submit for dssl-story framework.

---
+++ js/story.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var canvas = new fabric.CanvasEx('canvas');
+
+var tiDiv = document.getElementById("textInput");
+var tiForm = document.getElementById("nodeForm");
+
+var counter = 1;
+
+function collectNodeData(){
+  console.log(canvas.getActiveObject());
+   var title = document.getElementById("nodeTitle").value;
+   var text = document.getElementById("nodeText").value;
+   tiDiv.style.visibility = "hidden";
+}
+
+function generateNewNode() {
+  var textBox = new fabric.Rect({
+    fill: 'blue',
+    opacity: 0.4,
+    width: 80,
+    height: 80,
+    hasControls: false,
+    lockRotation: true,
+    hoverCursor: "default",
+    moveCursor: "default",
+    storyNode: true,
+    id: counter,
+  });
+  counter++;
+  canvas.add(textBox);
+}
+
+function closeNode() {
+  tiDiv.style.visibility = "hidden";
+  tiForm.reset();
+}
+
+function openNode() {
+    tiDiv.style.visibility = "visible";
+}
+
+canvas.on('mouse:dblclick', function (options) {
+  if (options.target && options.target !== null && options.target !== undefined && options.target.storyNode) {
+    openNode();
+  }
+});
